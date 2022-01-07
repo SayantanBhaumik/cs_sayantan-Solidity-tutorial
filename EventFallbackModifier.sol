ff15a5a11da3bd14cf1edda2083b5a722f636556
cs_sayantan <84985055+SayantanBhaumik@users.noreply.github.com>
2022-01-07 17:22:11 +0530

    Create EventFallbackModifier.sol

---
+++ EventFallbackModifier.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.6.0;
+
+contract Mycontract{
+
+    event senderLogger(address);
+    event valueLogger(uint256);
+
+    address private owner;
+
+    constructor () public {
+        owner=msg.sender;
+    }
+
+    //we are using modifier to restric iteraction only with owner of the smart contract
+    modifier onlyOwner{
+        require(msg.sender==owner,"not an owner");
+        //function execution
+        _ ;
+    }
+    //for checking the valid amount of ether sent to the contract
+    modifier validAmount{
+        require(msg.value >= 1 ether,"not sufficient amount");
+        _ ;
+    }
+    
+    //fallback function with payable modifier 
+    //no function identifier
+    //accepts no argument
+    //used to receive ethers without additional data to smart contract
+
+    function() payable onlyOwner validAmount  {
+        senderLogger(msg.sender);
+        valueLogger(msg.value);
+
+    }
+}
